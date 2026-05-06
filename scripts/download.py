f1d8b7e24f3fddb57623c4175e83074a1e8dca86
Arsam Aryandoust <arsam@mit.edu>
2026-05-06 19:05:38 +0200

    feat: download and contributions.md

--- scripts/download.py
+++ scripts/download.py
@@ -11,8 +11,26 @@ from aidotgrids import load
 sys.path.add('src')
 import utils
 
+MAX_WORKERS = 64
+MAX_WORKERS_DOWNLOAD = 32
 
 
 config = utils.parse_config('config.yml')
 
-ds = 
+print(config)
+
+def main():
+
+	ds = load.load_task(
+		task_name='WindFarm',
+		subtask_name='odd_time_predict48h',
+		root_path='/hpcwork/p0027791/AI-grids',
+		max_workers=MAX_WORKERS,
+		max_workers_download=MAX_WORKERS_DOWNLOAD,
+		data_frac=0.01
+	)
+
+
+if __name__ == '__main__':
+	pass
+	#main()
