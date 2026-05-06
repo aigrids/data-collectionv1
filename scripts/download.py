4cbc1a9145e9855c3fdeaafad408c5221de63bfa
Arsam Aryandoust <arsam@mit.edu>
2026-05-06 19:29:49 +0200

    fix: download script for all datasets

--- scripts/download.py
+++ scripts/download.py
@@ -8,29 +8,37 @@ Example usage:
 import sys
 from aidotgrids import load
 
-sys.path.add('src')
+sys.path.append('src')
 import utils
 
 MAX_WORKERS = 64
 MAX_WORKERS_DOWNLOAD = 32
+DATA_FRAC = 0.01
+MAP_TASK_TO_SAMPLE_SUBTASK = {
+	'OPFData': 'train_small_test_medium',
+	'PowerGraph': 'cascading_failure_binary',
+	'SolarCube': 'odd_time_area',
+	'BuildingElectricity': 'odd_time_buildings92',
+	'WindFarm': 'odd_time_predict48h'
+}
 
-
-config = utils.parse_config('config.yml')
-
-print(config)
 
 def main():
+	config = utils.parse_config('config.yml')
+	root = config['root_path_datasets']
 
-	ds = load.load_task(
-		task_name='WindFarm',
-		subtask_name='odd_time_predict48h',
-		root_path='/hpcwork/p0027791/AI-grids',
-		max_workers=MAX_WORKERS,
-		max_workers_download=MAX_WORKERS_DOWNLOAD,
-		data_frac=0.01
-	)
+	for task in config['list_avail_tasknames']:
+		subtask = MAP_TASK_TO_SAMPLE_SUBTASK[task]
+
+		ds = load.load_task(
+			task_name=task,
+			subtask_name=subtask,
+			root_path=root,
+			max_workers=MAX_WORKERS,
+			max_workers_download=MAX_WORKERS_DOWNLOAD,
+			data_frac=DATA_FRAC
+		)
 
 
 if __name__ == '__main__':
-	pass
-	#main()
+	main()
